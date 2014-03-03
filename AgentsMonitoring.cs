4d3a286ea425dcc8a313d2b161a877a4884e0c90
ludovic.ferre <ludovic.ferre@gmail.com>
2014-03-03 08:48:21 +0100

    Initial project setup.

---
+++ AgentsMonitoring.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Symantec.CWoC {
+    class AgentsMonitoring {
+        static void Main(string[] args) {
+        }
+    }
+}
