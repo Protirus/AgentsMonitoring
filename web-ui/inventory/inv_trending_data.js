2770f561ce96187a445b8512454d89b0b11bb8ec
ludovic.ferre <ludovic.ferre@gmail.com>
2014-03-06 16:32:12 +0100

    Renamed the inventory data file to inv_trending_data.js and modified the UI pages accordingly.

--- web-ui/inventory/inv_trending_data.js
+++ web-ui/inventory/inv_trending_data.js
@@ -1,20 +1,12 @@
-// Chart options
-var core_options = { title: 'Altiris Agent (7.1.15451.8460)' };
-var inv_options = { title: 'Inventory Agent (7.1.7875)' };
-var sua_options = { title: 'Software Update Agent (7.1.7875)' };
-var swm_options = { title: 'Software Management Agent (7.1.7858)' };
-
 var bi_options = { title: 'Basic Inventory (Core)' };
 var hw_options = { title: 'Hardware Inventory' };
 var os_options = { title: 'OS Inventory' };
 var sw_options = { title: 'Software Inventory' };
 var ug_options = { title: 'User Group Inventory' };
 
-
 var candlestick_all_options = {legend:'none', title:'Computers sending Inventory'};
 var candlestick_out_options = {legend:'none', title: 'Inventory data older than 4 weeks'};
 
-// Gauge control table
 var inv_gauge_table = [
 	['Label', 'Value'],
 	['Basic',  97.70],
@@ -23,14 +15,7 @@ var inv_gauge_table = [
 	['SW',  95.99],
 	['UG',  94.88]
 ];
-var agent_gauge_table = [
-	['Label', 'Value'],
-	['Core', 96.58],
-	['Inv.', 95.85],
-	['Patch', 94.17],
-	['Soft.', 96.22]
-];
-// INVENTORY Line chart tables
+
 var bi_table = [
 	['Date', 'Agent #', 'Updated', 'Outdated'],
 	['2014-02-25', 13307, 12902, 205],
@@ -76,44 +61,7 @@ var ug_table = [
 	['2014-03-01', 12802, 11835, 967],
 	['2014-03-02', 12795, 11832, 963]
 ];
-var core_table = [
-	['Date', 'Agent #', 'OK', 'NOK'],
-	['2014-02-25', 13271, 12428, 443],
-	['2014-02-26', 13292, 12494, 398],
-	['2014-02-27', 13296, 12558, 338],
-	['2014-02-28', 13289, 12586, 303],
-	['2014-03-01', 13248, 12584, 264],
-	['2014-03-02', 13242, 12833, 259]
-];
-var inv_table = [
-	['Date', 'Agent #', 'OK', 'NOK'],
-	['2014-02-25', 12135, 11168, 567],
-	['2014-02-26', 13162, 12247, 515],
-	['2014-02-27', 13178, 12308, 470],
-	['2014-02-28', 13178, 12342, 436],
-	['2014-03-01', 13137, 12337, 400],
-	['2014-03-02', 13146, 12352, 394]
-];
-var sua_table = [
-	['Date', 'Agent #', 'OK', 'NOK'],
-	['2014-02-25',12966,11665, 801],
-	['2014-02-26',12984,11729, 855],
-	['2014-02-27',12968,11788, 780],
-	['2014-02-28',12964,11818, 746],
-	['2014-03-01',12943,11833, 710],
-	['2014-03-02',12937,11833, 704]
-]
-var swm_table = [
-	['Date', 'Agent #', 'OK', 'NOK'],
-	['2014-02-25', 13130, 12184, 446],
-	['2014-02-26', 13149, 12248, 401],
-	['2014-02-27', 13264, 12453, 311],
-	['2014-02-28', 13260, 12492, 268],
-	['2014-03-01', 13219, 12486, 233],
-	['2014-03-02', 13213, 12486, 227]
-]
 
-// Candle stick tables
 var candlestick_all_table = [
 	['Basic Inventory', 13264, 13271, 13264, 13323],
 	['HW Inventory', 12773, 12802, 12795, 12830],
@@ -127,16 +75,4 @@ var candlestick_out_table = [
 	['OS Inventory', 964, 968, 964, 1196],
 	['SW Inventory', 733, 733, 755, 956],
 	['UG Inventory', 963, 967, 963, 1195],
-];
-var candlestick_agent_table = [
-	['Core', 13242, 13248, 13242, 13296],
-	['Inv.', 12135, 13137, 13146, 13178],
-	['Patch', 12937, 12943, 12937, 12984],
-	['Soft.', 13130, 13219, 13213, 13264]
-];
-var candlestick_agent_outdated_table = [
-	['Core', 659, 664, 659, 843],
-	['Inv.', 794, 800, 794, 967],
-	['Patch', 1104, 1110, 1104, 1301],
-	['Soft.', 727, 733, 727, 946]
 ];
