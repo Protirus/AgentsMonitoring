447b082fb552b0d714fef6cfb4b77306bd417bf2
somewhatsomewhere <somewhatsomewhere@users.noreply.github.com>
2018-02-01 10:59:13 +0100

    Adding 8.1 support and fixing hyphen...

--- build.bat
+++ build.bat
@@ -6,21 +6,26 @@ set adb=Altiris.Database
 set gac=C:\Windows\Microsoft.NET\assembly\GAC_MSIL
 set csc=@c:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe
 
+if "%1"=="8.1" goto build-8.1
+if "%1"=="8.0" goto build-8.0
 if "%1"=="7.6" goto build-7.6
 if "%1"=="7.5" goto build-7.5
 
 :default build path
+
+:build-8.1
+set build=8.1
+set ver1=v4.0_8.1.4528.0__d516cb311cfb6e4f
+goto build
+
+:build-8.0
 set build=-8.0
-
 set ver1=v4.0_8.0.2298.0__d516cb311cfb6e4f
-
 goto build
 
 :build-7.6
 set build=-7.6
-
 set ver1=v4.0_7.6.1383.0__d516cb311cfb6e4f
-
 goto build
 
 
@@ -35,5 +40,5 @@ set ver1=7.5.3153.0__d516cb311cfb6e4f
 
 :build
 : Build AgentsMonitoring.exe
-cmd /c %csc% /reference:%gac%\%ans%\%ver1%\%ans%.dll /reference:%gac%\%adb%\%ver1%\%adb%.dll /out:AgentsMonitoring%build%.exe AgentsMonitoring.cs APIWrapper.cs
+cmd /c %csc% /reference:%gac%\%ans%\%ver1%\%ans%.dll /reference:%gac%\%adb%\%ver1%\%adb%.dll /out:AgentsMonitoring-%build%.exe AgentsMonitoring.cs APIWrapper.cs
 cmd /c %csc% /reference:%gac%\%ans%\%ver1%\%ans%.dll /reference:%gac%\%adb%\%ver1%\%adb%.dll /out:InventoryMonitoring-%build%.exe InventoryMonitoring.cs APIWrapper.cs
